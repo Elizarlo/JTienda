4524639896ec5c72244eff221d2f4d088af3d361
Elizarlo <eluisramos0909@gmail.com>
2019-01-30 06:08:18 +0000

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-26 20:54:09 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-26T20:54:09+00:00	INFO 187.198.66.45	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-26T20:54:18+00:00	INFO 187.198.66.45	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-26T20:54:25+00:00	INFO 187.198.66.45	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-28T03:59:50+00:00	INFO 189.218.31.89	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-01-29T14:59:37+00:00	INFO 200.95.170.220	joomlafailure	Username and password do not match or you do not have an account yet.
